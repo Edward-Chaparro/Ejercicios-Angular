a94bedcc2c4e5571affcfca4a9ff0fbf20c66707
gitedward7 <108356585+gitedward7@users.noreply.github.com>
2022-09-09 16:43:17 -0500

    add query and image routes

--- src/app/gifts/services/gifs.service.ts
+++ src/app/gifts/services/gifs.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-
+  
+  private apiKey: string = "5P6ZZM7TQQnhIwAzBZoPPvriOV72X3Rt"
   private _historial: string [] = [];
+
+  public resultados: any[] = [];
 
   get historial () {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
 
   buscarGifs (query: string = '') {
 
@@ -18,8 +24,12 @@ export class GifsService {
     if(!this._historial.includes (query)) {
       this._historial.unshift (query);
       this._historial = this._historial.splice (0,10);
-    }
-
-    console.log(this._historial);
+    } 
+    
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=5P6ZZM7TQQnhIwAzBZoPPvriOV72X3Rt	&q=${ query }&limit=10`)
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
   }
 }
